15e314cd3ccdd95c1d3570ddfe6b763075bef5b6
rbsjsu <119709176+rbsjsu@users.noreply.github.com>
2022-12-03 05:15:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,3 +11,13 @@ df = pd.DataFrame({
 })
 
 df
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
